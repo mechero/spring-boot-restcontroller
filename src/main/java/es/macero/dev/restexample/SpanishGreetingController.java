4a62af0de0f8c49b87a92385fd0791136c5e1de3
Moisés Macero <moises.macero@gmail.com>
2017-03-04 17:23:28 +0100

    Using plural instead

--- src/main/java/es/macero/dev/restexample/SpanishGreetingController.java
+++ src/main/java/es/macero/dev/restexample/SpanishGreetingController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Random;
 
 @RestController
-@RequestMapping("/spanish-greeting")
+@RequestMapping("/spanish-greetings")
 public class SpanishGreetingController {
 
     private List<SpanishGreeting> spanishGreetings;
